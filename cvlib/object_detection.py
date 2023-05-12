18e52934b43288155dccd892386f6146c5b05870
randhana <pulathisi1921@gmail.com>
2023-05-12 20:41:26 +0530

    updated  cvlib/object_detection.py

--- cvlib/object_detection.py
+++ cvlib/object_detection.py
@@ -13,48 +13,23 @@ net = None
 dest_dir = os.path.expanduser('~') + os.path.sep + '.cvlib' + os.path.sep + 'object_detection' + os.path.sep + 'yolo' + os.path.sep + 'yolov3'
 classes = None
 #colors are BGR instead of RGB in python
-COLORS = [0,0,255], [255,0,0]
+#COLORS = [0,0,255], [255,0,0]
+COLORS = [(0, 255, 0), (0, 0, 255), (255, 255, 255)]  # Add a third color (white) to the list
+
 
 def populate_class_labels():
 
-    #we are using a pre existent classifier which is more reliable and more efficient than one
-    #we could make using only a laptop
-    #The classifier should be downloaded automatically when you run this script
-    
-    
     class_file_name = 'yolov3_classes.txt'
     class_file_abs_path = dest_dir + os.path.sep + class_file_name
-    url = 'https://github.com/Nico31415/Drowning-Detector/raw/master/yolov3.txt'
+    url = 'https://github.com/randhana/Drowning-Detection-/blob/master/yolov3.txt'
     if not os.path.exists(class_file_abs_path):
         download_file(url=url, file_name=class_file_name, dest_dir=dest_dir)
     f = open(class_file_abs_path, 'r')
     classes = [line.strip() for line in f.readlines()]
     return classes
 
-
-
-    # for populating class labels using the yolo.txt file stored in the working directory
-     # Set the destination directory to the current working directory
-
-'''
-  
-    dest_dir = os.getcwd()
-
-    # Define the name and absolute path of the class file
-    class_file_name = 'yolo.txt'
-    class_file_abs_path = dest_dir + os.path.sep + class_file_name
-
-    # Read the class labels from the file
-    f = open(class_file_abs_path, 'r')
-    classes = [line.strip() for line in f.readlines()]
-'''
-    
-
-
 def get_output_layers(net):
 
-    #the number of output layers in a neural network is the number of possible
-    #things the network can detect, such as a person, a dog, a tie, a phone...
     layer_names = net.getLayerNames()
 
     output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
@@ -73,22 +48,22 @@ def draw_bbox(img, bbox, labels, confidence, Drowning, write_conf=False):
 
     for i, label in enumerate(labels):
 
-        #if the person is drowning, the box will be drawn red instead of blue
+        #if the person is drowning, the box will be drawn red instead of green
         if label == 'person' and Drowning:
-            color = COLORS[0]
-            label = 'DROWNING'
-            ser.write(b'on')  # send the string "on" followed by a newline character
-            # start playing the sound in a new thread
+            color = COLORS[1]
+            label = 'ALERT DROWNING'
+            ser.write(b'on')  # send the string "on" 
+            
             threading.Thread(target=play_sound).start()
         else:
-            color = COLORS[1]
-            label = 'NOT DROWNING'
+            color = COLORS[0]
+            label = 'Normal'
 
         if write_conf:
             label += ' ' + str(format(confidence[i] * 100, '.2f')) + '%'
 
 
-        #you only need to points (the opposite corners) to draw a rectangle. These points
+        #only need to points (the opposite corners) to draw a rectangle. These points
         #are stored in the variable bbox
         cv2.rectangle(img, (bbox[i][0],bbox[i][1]), (bbox[i][2],bbox[i][3]), color, 2)
 
@@ -103,7 +78,6 @@ def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
     else:
         pass
 
-    
     scale = 0.00392
 
     global classes
@@ -119,7 +93,7 @@ def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
     weights_file_name = 'yolov3.weights'
     weights_file_abs_path = dest_dir + os.path.sep + weights_file_name
 
-    url = 'https://github.com/Nico31415/Drowning-Detector/raw/master/yolov3.cfg'
+    url = 'https://github.com/randhana/Drowning-Detection-/blob/master/yolov3.cfg'
 
     if not os.path.exists(config_file_abs_path):
         download_file(url=url, file_name=config_file_name, dest_dir=dest_dir)
@@ -193,5 +167,4 @@ def detect_common_objects(image, confidence=0.5, nms_thresh=0.3):
     return bbox, label, conf
 
 def play_sound():
-    # replace 'path/to/file.mp3' with the actual path to your mp3 file
     playsound('sound/alarm.mp3')
